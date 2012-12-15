a2fd421fddeb0650d818bba0423341dc28f23cbc
marcelofernandes <marcelo.fernandes@dce.ufpb.br>
2012-12-15 21:53:21 -0200

    Update exercicio7/Agenda.java

--- exercicio7/Agenda.java
+++ exercicio7/Agenda.java
@@ -14,11 +14,19 @@ public class Agenda implements AgendaIF{
 	private List<Contato> contatos;
 	private GravadorDeContatosList gravador;
 
+	/**
+	 * Construtor padrão que inicializa as variáveis.
+	 * 
+	 */
 	public Agenda(){
 		gravador = new GravadorDeContatosList();
 		carregaContatos();
 	}
 
+	/**
+	 * Método para gravar os contatos no arquivo.
+	 * 
+	 */
 	private void gravaContatos(){
 		try{
 			gravador.gravaContato(contatos);
@@ -29,6 +37,11 @@ public class Agenda implements AgendaIF{
 		
 		
 	}
+	
+	/**
+	 * Método para carregar os contatos no arquivo.
+	 * 
+	 */
 	private void carregaContatos(){
 		try{
 			contatos = gravador.leContato();
@@ -39,6 +52,14 @@ public class Agenda implements AgendaIF{
 		}
 	}
 
+	/**
+	 * Método para adicionar um contato.
+	 * 
+	 *@param nome O nome do contato a ser adicionado.
+	 * 
+	 * @param tel O telefone do contato a ser adicionado.
+	 * 
+	 */
 	public void adicionarContato(String nome, String tel){
 		for(Contato c: this.contatos){
 			if(c.getNome().equals(nome)){
@@ -51,6 +72,15 @@ public class Agenda implements AgendaIF{
 		gravaContatos();
 		System.out.println("Contato "  + nome + " adicionado.");
 	}
+	
+	/**
+	 * Método para remover um contato.
+	 * 
+	 * @param nomeContato O nome do contato a ser removido.
+	 * 
+	 * @throws ContatoInexistenteException Exceção que será lançada caso o contato não exista.
+	 * 
+	 */
 	public void removerContato(String nomeContato)throws ContatoInexistenteException{
 		for(Contato c: this.contatos){
 			if( c.getNome().equals(nomeContato) ){
@@ -62,6 +92,17 @@ public class Agenda implements AgendaIF{
 		}
 		throw new ContatoInexistenteException("Contato " + nomeContato + " nao encontrado.");
 	}
+	
+	/**
+	 * Método para pesquisar um determinado contato.
+	 * 
+	 * @param nomeContato O nome do contato a ser pesquisado.
+	 * 
+	 * @return Retorna o contato pesquisado.
+	 * 
+	 * @throws ContatoEnexistenteException Exceção que será lançada caso o contato não exista.
+	 * 
+	 */
 	public Contato pesquisarContato(String nomeContato)throws ContatoInexistenteException{
 		for(Contato c: this.contatos){
 			if( c.getNome().equals(nomeContato) ){
