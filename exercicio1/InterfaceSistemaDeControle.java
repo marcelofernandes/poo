1c5def0bef4be844a4a8a973db0e2f6b1845ee58
marcelofernandes <marcelo.fernandes@dce.ufpb.br>
2012-12-15 20:57:40 -0200

    Update exercicio1/InterfaceSistemaDeControle.java

--- exercicio1/InterfaceSistemaDeControle.java
+++ exercicio1/InterfaceSistemaDeControle.java
@@ -1,6 +1,36 @@
 import java.util.List;
+
+/**
+ * Interface par controle de pedidos.
+ * 
+ * @autor Marcelo Fernandes
+ * 
+ */
 public interface InterfaceSistemaDeControle{
+	
+	/**
+	 * Metodo para aicionar um pedido.
+	 * 
+	 * @param p Pedido a ser adicionado.
+	 * 
+	 */
 	public void adicionaPedido(Pedido p);
+	
+	/**
+	 * Metodo que pesquisa determinados pedidos.
+	 * 
+	 * @param codProduto O código do produto a ser pesquisado.
+	 * 
+	 * @return Retorna uma lista com os pedidos encontrados.
+	 * 
+	 */
 	public List<Pedido> pesquisaPedidosIncuindoProduto(long codProduto);
+	
+	/**
+	 * Metodo para remover um determinado pedido.
+	 * 
+	 * @param numPedido O numero do pedido a ser removido
+	 * 
+	 */
 	public void removePedido(long numPedido);
 }
