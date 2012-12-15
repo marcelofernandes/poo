6630b540f485e52ac8c746baf553b462368e3c52
marcelofernandes <marcelo.fernandes@dce.ufpb.br>
2012-12-15 21:25:46 -0200

    Update exercicio3/ControleDePedidos.java

--- exercicio3/ControleDePedidos.java
+++ exercicio3/ControleDePedidos.java
@@ -1,14 +1,31 @@
 import java.util.*;
 
-
+/**
+ * Classe para controle de pedidos.
+ * 
+ * @author Marcelo Fernandes
+ * 
+ */
 public class ControleDePedidos implements InterfaceSistemaDeControle {
 
 	private List <Pedido> pedidos;
 	
+	/**
+	 * Construtor para que inicializa a variável pedidos.
+	 * 
+	 */
 	public ControleDePedidos(){
 		this.pedidos = new ArrayList<Pedido>();
 	}
 	
+	/**
+	 * Método que calcula a quantidade de pedidos de determinado cliente.
+	 * 
+	 * @param nomeCliente O nome do cliente cuja quantidade de pedidos vai ser calculada.
+	 * 
+	 * @return Retorna a quantidade de pedidos desse cliente.
+	 * 
+	 */
 	public int calculaQuantidadeDePedidosDoCliente(String nomeCliente){
 		int cont = 0;
 		for (Pedido p: this.pedidos){
@@ -19,12 +36,26 @@ public class ControleDePedidos implements InterfaceSistemaDeControle {
 		return cont;
 	}
 	
+	/**
+	 * Método para adicionar um pedido.
+	 * 
+	 * @param p O pedido a ser adicionado.
+	 * 
+	 */
 	@Override
 	public void adicionaPedido(Pedido p) {
 		this.pedidos.add(p);
 
 	}
 
+	/**
+	 * Método para pesquisar pedidos que possuem determinado produto.
+	 * 
+	 * @param codProduto O codigo do produto a ser pesquisado.
+	 * 
+	 * return Retorna a lista com os pedidos que possuem o produto.
+	 * 
+	 */
 	@Override
 	public List<Pedido> pesquisaPedidosIncluindoProduto(String codProduto) {
 		List <Pedido> pedidosProcurados = new LinkedList<Pedido>();
@@ -39,6 +70,12 @@ public class ControleDePedidos implements InterfaceSistemaDeControle {
 		return pedidosProcurados;
 	}
 
+	/**
+	 * Método para remover um pedido.
+	 * 
+	 * @param numeroPedido O numero do pedido a ser removido.
+	 * 
+	 */
 	@Override
 	public void removePedido(long numeroPedido) {
 		for (Pedido p: this.pedidos){
